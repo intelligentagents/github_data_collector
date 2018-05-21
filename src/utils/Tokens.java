239ef804a7b2e6449653f5d1e4d6df947e7293fc
Caio Barbosa <cbvs@ic.ufal.br>
2018-05-21 13:03:15 -0300

    add tokens.java to gitignore

--- src/utils/Tokens.java
+++ src/utils/Tokens.java
@@ -2,13 +2,30 @@ package utils;
 
 public class Tokens {
 
-<<<<<<< HEAD
-
-
-	
-=======
 	//public static final String USERNAME = "Github Username";
 	//public static final String PASSWORD = "OAuth Access Token";
-		
->>>>>>> c5d6b0afcb962b16a6415e8a4f848fe59a987b02
+	
+	//public static final String USERNAME = "guriosam";
+	//public static final String PASSWORD = "a91507629423c413809eb5b672760589b9f3027a";
+	
+	//public static final String USERNAME = "andersonss";
+	//public static final String PASSWORD = "";
+	
+	//public static final String USERNAME = "danieltmo";
+	//public static final String PASSWORD = "fc4b5e524a175873520927f8668f1f3e97abec46";
+	
+	//public static final String USERNAME = "filipefalcaos";
+	//public static final String PASSWORD = "9a1540d269f52746f7df18d0d9bdec308bce3f19";
+
+	//public static final String USERNAME = "francamacdowell";
+	//public static final String PASSWORD = "7fb8a77bdfc16f5cbab5e7c4a6252ff204cd6d6a";
+	
+	//public static final String USERNAME = "r4phael";
+	//public static final String PASSWORD = "40c7371b84d7691263e534d6f31dfebbc6174c61";
+	
+	//public static final String USERNAME = "egabrielnunes";
+	//public static final String PASSWORD = "7df3442dc884f28d52bc341f0d498205b558e127";
+	 
+	
+
 }
